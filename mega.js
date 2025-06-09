6340b7713c8d4b202fc271685c7383fb036c4ff0
Dasun200710 <pk4245477@gmail.com>
2025-06-09 15:09:51 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'sulakshamadara4@gmail.com',
-    password: 'sulaMd0310md@',
+    email: 'gdasun687@gmail.com',
+    password: 'https://dassa-profile-web.vercel.app/',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
